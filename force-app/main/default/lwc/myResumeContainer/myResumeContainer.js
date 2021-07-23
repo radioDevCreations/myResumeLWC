9cbe5dc447e7fdfca51dae59f9775381119e7a01
radioDevCreations <radekrozmus@gmail.com>
2021-07-23 14:18:33 +0200

    education completed

--- force-app/main/default/lwc/myResumeContainer/myResumeContainer.js
+++ force-app/main/default/lwc/myResumeContainer/myResumeContainer.js
@@ -1,5 +1,5 @@
 import { LightningElement, track } from 'lwc';
-import { SOCIAL_LINKS, USER_DETAILS, PROFILE_IMAGE, CAREER_SUMMARY, EXPERIENCE_DATA } from './myResumeContainerData';
+import { SOCIAL_LINKS, USER_DETAILS, PROFILE_IMAGE, CAREER_SUMMARY, EXPERIENCE_DATA, EDUCATION_DATA } from './myResumeContainerData';
 
 
 export default class MyResumeContainer extends LightningElement {
@@ -8,4 +8,5 @@ export default class MyResumeContainer extends LightningElement {
     @track PROFILE_IMAGE = PROFILE_IMAGE;
     @track CAREER_SUMMARY = CAREER_SUMMARY;
     @track EXPERIENCE_DATA = EXPERIENCE_DATA;
+    @track EDUCATION_DATA = EDUCATION_DATA;
 }
